7c079da98449d77885f4e51e007ce262694e3e13
Anatole <anatole@xiantis.net>
2021-03-04 08:58:31 +0100

    ClientService - extend UserStructure

--- src/app/service/client.service.ts
+++ src/app/service/client.service.ts
@@ -1,44 +1,20 @@
 
 
 import { Injectable } from '@angular/core';
-import { Constants } from '@typings/src/Constants';
 import { DataStructure } from '@typings/typings/DataStructure';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
 import { LoggerService } from 'src/app/service/logger.service';
+import { UserStructure } from 'src/app/util/user.structure';
 
 @Injectable({providedIn: 'root'})
-export class ClientService {
+export class ClientService extends UserStructure {
 	private token = '';
 	private authState = new BehaviorSubject<boolean>(false);
-	private data = new BehaviorSubject<DataStructure.TwitchUser | null>(null);
 
 	constructor(
 		private logger: LoggerService
-	) {}
-
-	getID(): Observable<string | null> {
-		return this.data.pipe(
-			map(data => !!data?._id  ? String(data._id) : null)
-		);
-	}
-
-	/**
-	 * Get the username of the client user
-	 */
-	getUsername(): Observable<string | null> {
-		return this.data.pipe(
-			map(data => data?.display_name ?? null)
-		);
-	}
-
-	/**
-	 * Get an URL to the avatar of the client user
-	 */
-	getAvatarURL(): Observable<string | null> {
-		return this.data.pipe(
-			map(data => data?.profile_image_url ?? null)
-		);
+	) {
+		super();
 	}
 
 	/**
@@ -48,8 +24,8 @@ export class ClientService {
 	 * @param data Twitch User data
 	 */
 	pushData(data: DataStructure.TwitchUser | null): void {
+		super.pushData(data);
 		if (!!data?._id) {
-			this.data.next(data);
 			this.setAuthState(!!data);
 			this.logger.info(`Signed in as ${data.display_name}.`);
 		} else {
@@ -64,15 +40,6 @@ export class ClientService {
 	}
 	getToken(): string {
 		return this.token;
-	}
-
-	/**
-	 * Get the user's rank
-	 */
-	getRank(): Observable<Constants.Users.Rank> {
-		return this.data.pipe(
-			map(data => data?.rank ?? Constants.Users.Rank.DEFAULT)
-		);
 	}
 
 	/**
---
+++ src/app/util/user.structure.ts
@@ -0,0 +1,51 @@
+import { Constants } from '@typings/src/Constants';
+import { DataStructure } from '@typings/typings/DataStructure';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
+export class UserStructure {
+	protected data = new BehaviorSubject<DataStructure.TwitchUser | null>(null);
+
+	/**
+	 * Push data onto this user.
+	 *
+	 * @param data Twitch User data
+	 */
+	 pushData(data: DataStructure.TwitchUser | null): void {
+		this.data.next(data);
+	}
+
+	getID(): Observable<string | null> {
+		return this.data.pipe(
+			map(data => !!data?._id  ? String(data._id) : null)
+		);
+	}
+
+	/**
+	 * Get the username of the client user
+	 */
+	getUsername(): Observable<string | null> {
+		return this.data.pipe(
+			map(data => data?.display_name ?? null)
+		);
+	}
+
+	/**
+	 * Get an URL to the avatar of the client user
+	 */
+	getAvatarURL(): Observable<string | null> {
+		return this.data.pipe(
+			map(data => data?.profile_image_url ?? null)
+		);
+	}
+
+	/**
+	 * Get the user's rank
+	 */
+	getRank(): Observable<Constants.Users.Rank> {
+		return this.data.pipe(
+			map(data => data?.rank ?? Constants.Users.Rank.DEFAULT)
+		);
+	}
+}

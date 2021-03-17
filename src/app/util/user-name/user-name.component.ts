d5d9245c4cadb983a03ed4768420ac2563f69161
Anatole <anatole@xiantis.net>
2021-03-17 07:08:25 +0100

    UserNameComponent - options user, avatarBorder, clickable, size

--- src/app/util/user-name/user-name.component.ts
+++ src/app/util/user-name/user-name.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ClientService } from 'src/app/service/client.service';
 import { ThemingService } from 'src/app/service/theming.service';
+import { UserStructure } from 'src/app/util/user.structure';
 
 
 @Component({
@@ -9,16 +10,25 @@ import { ThemingService } from 'src/app/service/theming.service';
 	styleUrls: ['./user-name.component.scss']
 })
 export class UserNameComponent implements OnInit {
+	@Input() user: UserStructure | undefined;
 	@Input() showAvatar = true;
+	@Input() avatarBorder = true;
 	@Input() showUsername: boolean | null = true;
+	@Input() clickable = true;
 
+	/** [avatar size, font size]  */
+	@Input() size: [number, number] = [2, .75];
 
 	constructor(
 		public clientService: ClientService,
 		public themingService: ThemingService
-	) {
+	) {}
 
-
+	/**
+	 * Get the user targeted in this component. If it was not specified, default to client user
+	 */
+	get target(): UserStructure {
+		return this.user ?? this.clientService;
 	}
 
 	ngOnInit(): void {

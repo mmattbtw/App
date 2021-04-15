6c411a88212f20692881df14356c969d7cec5cbc
Anatole <anatole@xiantis.net>
2021-04-16 01:49:42 +0200

    About - initial layout

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemingService } from 'src/app/service/theming.service';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+	selector: 'app-about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+	constructor(
+		public themingService: ThemingService
+	) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { AboutRouterModule } from 'src/app/about/about-routing.module';
 
 import { AboutComponent } from 'src/app/about/about.component';
+import { MaterialModule } from 'src/app/material.module';
+import { UtilModule } from 'src/app/util/util.module';
 
 @NgModule({
 	imports: [
-		AboutRouterModule
+		AboutRouterModule,
+		MaterialModule,
+		UtilModule
 	],
 	exports: [],
 	declarations: [AboutComponent],

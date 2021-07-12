ba20f1595a578c0f6e2e05d2808d364b7d95adcb
Anatole <me@anatole.dev>
2021-07-12 06:20:43 +0200

    Automate Update Dialog (#62)

--- src/app/update-dialog.component..ts
+++ src/app/update-dialog.component..ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { BehaviorSubject, interval, } from 'rxjs';
+import { switchMap, take, tap } from 'rxjs/operators';
 
 @Component({
 	selector: 'app-update-dialog',
@@ -6,18 +9,34 @@ import { Component, OnInit } from '@angular/core';
 		<h1 mat-dialog-title> Update Available </h1>
 
 		<div mat-dialog-content>
-			<p>An update for 7tv.app is available. Would you like to download and refresh the page?</p>
+			<p>An update for 7tv.app is available. The page will refresh automatically in a few seconds</p>
 		</div>
 
 		<div mat-dialog-actions>
-			<button mat-raised-button color="accent" [matDialogClose]="true">UPDATE</button>
+			<button mat-raised-button color="accent" [matDialogClose]="true">
+				UPDATE ({{ autoSeconds | async }})
+			</button>
 			<button mat-flat-button [matDialogClose]="false">NOT NOW</button>
 		<div>
 	`
 })
 
 export class UpdateDialogComponent implements OnInit {
-	constructor() { }
+	autoSeconds = new BehaviorSubject<number>(AUTO_UPDATE_SECONDS);
+
+	constructor(dialogRef: MatDialogRef<UpdateDialogComponent>) {
+		interval(1e3).pipe(
+			take(AUTO_UPDATE_SECONDS),
+			switchMap(() => this.autoSeconds.pipe(take(1))),
+			tap(n => this.autoSeconds.next(n - 1))
+		).subscribe({
+			complete: () => {
+				dialogRef.close(true);
+			}
+		});
+	}
 
 	ngOnInit(): void { }
 }
+
+const AUTO_UPDATE_SECONDS = 3;

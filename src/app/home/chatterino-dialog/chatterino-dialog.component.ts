3b97779869ea5ada40556a5fba446b3471559ffc
Anatole <me@anatole.dev>
2021-08-06 16:38:43 +0200

    Chatterino Dialog - Make data-driven

--- src/app/home/chatterino-dialog/chatterino-dialog.component.ts
+++ src/app/home/chatterino-dialog/chatterino-dialog.component.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatMenu } from '@angular/material/menu';
-import { map } from 'rxjs/operators';
+import { asapScheduler, scheduled } from 'rxjs';
+import { concatAll, map } from 'rxjs/operators';
+import { RestService } from 'src/app/service/rest.service';
 
 @Component({
 	selector: 'app-chatterino-dialog',
@@ -15,7 +16,7 @@ export class ChatterinoDialogComponent implements OnInit {
 	windowsDownloads = [] as ChatterinoDialogComponent.WindowsDownload[];
 
 	constructor(
-		private http: HttpClient
+		private restService: RestService
 	) { }
 
 	download(ev: MouseEvent, url: string | undefined): void {
@@ -27,40 +28,48 @@ export class ChatterinoDialogComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
+		scheduled([
+			this.restService.createRequest<ChatterinoDialogComponent.ChatterinoVersion>('get', '/chatterino/version/win/stable', {}, 'v2').pipe(
+				RestService.onlyResponse(),
+				map(res => {
+					this.windowsDownloads.push(
+						{ label: 'INSTALLER', url: res.body?.download ?? '' },
+						{ label: 'PORTABLE / STANDALONE EXE', url: res.body?.portable_download ?? '' }
+					);
+
+					return {
+						label: 'Windows', icon: 'windows', svgIcon: true,
+						menu: this.installTypeMenu
+					} as ChatterinoDialogComponent.PlatformIcon;
+				})
+			),
+
+			this.restService.createRequest<ChatterinoDialogComponent.ChatterinoVersion>('get', '/chatterino/version/linux/stable', {}, 'v2').pipe(
+				RestService.onlyResponse(),
+				map(res => ({
+					label: 'Linux', icon: 'linux', svgIcon: true,
+					url: res.body?.download
+				} as ChatterinoDialogComponent.PlatformIcon))
+			),
+
+			this.restService.createRequest<ChatterinoDialogComponent.ChatterinoVersion>('get', '/chatterino/version/macos/stable', {}, 'v2').pipe(
+				RestService.onlyResponse(),
+				map(res => ({
+					label: 'MacOS', icon: 'apple', svgIcon: true,
+					url: res.body?.download
+				}))
+			)
+		], asapScheduler).pipe(
+			concatAll()
+		).subscribe({
+			next: p => this.platforms.push(p)
+		});
+
 		this.platforms.push(
-			{
-				label: 'Linux',
-				icon: 'linux',
-				svgIcon: true,
-				url: 'https://github.com/SevenTV/chatterino7/releases/download/7.3.3/Chatterino-x86_64.AppImage'
-			},
-			{
-				label: 'Windows',
-				icon: 'windows',
-				svgIcon: true,
-				menu: this.installTypeMenu
-			},
-			{
-				label: 'MacOS',
-				icon: 'apple',
-				svgIcon: true,
-				url: 'https://github.com/SevenTV/chatterino7/releases/download/7.3.3/Chatterino.dmg'
-			},
 			{
 				label: 'Nightly Build',
 				icon: 'nightlight',
 				url: 'https://github.com/SevenTV/chatterino7/releases/tag/nightly-build'
-			}
-		);
-
-		this.windowsDownloads.push(
-			{
-				label: 'INSTALLER',
-				url: 'https://github.com/SevenTV/chatterino7/releases/download/7.3.3/Chatterino.Installer.exe'
-			},
-			{
-				label: 'PORTABLE / STANDALONE EXE',
-				url: 'https://github.com/SevenTV/chatterino7/releases/download/7.3.3/Chatterino.Portable.zip'
 			}
 		);
 	}
@@ -79,5 +88,11 @@ export namespace ChatterinoDialogComponent {
 	export interface WindowsDownload {
 		label: string;
 		url: string;
+	}
+
+	export interface ChatterinoVersion {
+		download: string;
+		portable_download?: string;
+		version: string;
 	}
 }
